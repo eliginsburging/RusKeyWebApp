1413f87bfc10a809d8b7879df0f984feaf14ee50
eli <eli.ginsburg.marcy@gmail.com>
2018-02-24 12:01:43 -0600

    created generic template and home template

--- ruskeyverbs/admin.py
+++ ruskeyverbs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Verb, Example, PerformancePerExample
 
 # Register your models here.
+admin.site.register(Verb)
+admin.site.register(Example)
+admin.site.register(PerformancePerExample)
--- ruskeyverbs/views.py
+++ ruskeyverbs/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Verb, Example, PerformancePerExample
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Placeholder for Verb Browser")
+    """
+    View function for home page
+    """
+    num_verbs = Verb.objects.all().count()
+    num_examples = Example.objects.all().count()
+    return render(
+        request,
+        'ruskeyverbs/index.html',
+        context={'num_verbs': num_verbs, 'num_examples': num_examples}
+    )
