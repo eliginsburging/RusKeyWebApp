e4dd4db084a6940313df884f6c8ede8fe8de46c0
eli <eli.ginsburg.marcy@gmail.com>
2018-07-29 13:41:38 -0500

    disabled django-heroku as it was maybe causing 500 error

--- RusKey/settings.py
+++ RusKey/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.horkuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 SECRET_KEY = os.environ.get('django_secretkey', '')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_access_keyid_ruskey', '')
@@ -155,4 +155,4 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 # )
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
